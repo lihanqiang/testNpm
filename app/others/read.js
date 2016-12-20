244779646e1754d5191937bb6d4b3b578d500b63
li <1162817095@qq.com>
2016-12-20 16:23:05 +0800

    add files

---
+++ app/others/read.js
@@ -0,0 +1,17 @@
+display: flex; // 块级元素的flex
+
+display: inline-flex; //行类元素的flex；
+
+设置了flex属性的box，可以设置6中属性
+
+flex-direction
+flex-wrap
+flex-flow
+justify-content
+align-items
+align-content
+
+
+flex-direction: 
+
+row | row-reverse | column | column-reverse; 水平 反向水平 垂直 反向垂直
